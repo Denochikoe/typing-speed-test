6e2a7cb445c4c00c61fa4d5fcee897729874f24d
Denochikoe <denochikoe@gmail.com>
2022-06-06 09:39:43 +0300

    First release of typing speed test

---
+++ script.js
@@ -0,0 +1,85 @@
+//log info(string,variables,object,regExp,array,function,events)
+//console.log("test");
+//let name = "john";
+//console.log(name);
+//console.log("%s %s",name,"smith");
+//console.log("%s %s",name,"smith",{age:25});
+//const obj = {name:"john",age:25};
+//console.log(obj);
+//console.log(obj.name + " " + obj.age);
+//function addition(a,b){
+    //return a+b;
+//}
+//console.log(addition(1,2));
+//function submit(){
+    //console.log("submitted");
+//} 
+
+//output(variables,object,array,function,events)
+//document.getElementById("test_button").innerHTML=add(1,2);
+//function submit_(){
+   // document.getElementById("test_button").addEventListener(click,submit_).innerHTML="submitted";
+//}
+//DOM
+//document.getElementById("test_button").innerHTML="test";
+
+//button
+//const x = document.getElementById("reset");
+//x.addEventListener("click",myFunction);
+//function myFunction(){
+    //document.getElementById("test_button").innerHTML="submitted";
+//}
+//function myFunction(e){
+    //e.preventDefault();
+   //var x = document.getElementById("text_").value;
+   //document.getElementById("test_button").innerHTML=x;
+//}
+
+// function myFunction(e){
+//     e.preventDefault();
+//    console.log(x);
+    
+// }
+// function myFunction(e){
+//     e.preventDefault();
+//     let count,i,j,y= document.querySelector("h3");
+//     let x = document.getElementById("text_").value;
+//     const arry = x.split(" ");
+//     const arry2= y.split(" ");
+//     for(let i=0;i<x.length;i++){
+//         for(let j=0;j<y.length;j++){
+//             if(x[i]!=y[j]){
+//                 ++count;
+//                 document.getElementById("mistakes_").innerHTML=count;
+//             }
+//         }
+//     }
+// }
+const  text = "The quick brown fox jumps over the lazy dog.";
+let str= text.split(" ");
+console.log(str);
+let text2= document.getElementById("text_").value;
+console.log(text2);
+const text4=[];
+    let text3= document.querySelector("h3").value;
+console.log(text3);
+ text4= text3.split(" ");
+console.log(text4);
+
+
+
+//eventlistener
+
+//create element
+//change element
+//style elements
+
+
+
+
+
+
+
+
+
+
